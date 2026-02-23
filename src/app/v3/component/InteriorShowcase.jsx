f765e2fd399ea741b3590482e37bdda42bb09397
harshrao26 <harshurao058@gmail.com>
2026-02-23 10:33:51 +0530

    ...

--- src/app/v3/component/InteriorShowcase.jsx
+++ src/app/v3/component/InteriorShowcase.jsx
@@ -53,7 +53,7 @@ const InteriorShowcase = () => {
                     <div className="relative aspect-video w-full rounded-[40px] md:rounded-[80px] overflow-hidden shadow-[0_32px_64px_-16px_rgba(0,0,0,0.25)] border border-gray-100">
                         <div className="absolute inset-0 pointer-events-none">
                             <iframe
-                                src="https://www.youtube.com/embed/eOK6gwjKXZo?autoplay=1&mute=1&loop=1&playlist=eOK6gwjKXZo&controls=0&rel=0&showinfo=0&iv_load_policy=3&modestbranding=1"
+                                src="https://www.youtube.com/embed/eOK6gwjKXZo?autoplay=1&loop=1&playlist=eOK6gwjKXZo&controls=0&rel=0&showinfo=0&iv_load_policy=3&modestbranding=1"
                                 className="absolute top-1/2 left-1/2 w-[101%] h-[101%] -translate-x-1/2 -translate-y-1/2 object-contain border-0"
                                 allow="autoplay; encrypted-media"
                                 title="Interior Showcase"
